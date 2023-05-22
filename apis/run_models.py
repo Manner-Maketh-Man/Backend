6bfb2dcf133c55cfbd9c46996f76a3bc8229b3e7
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2023-05-23 01:37:03 +0900

    added kobert codes updated ensemble codes

--- apis/run_models.py
+++ apis/run_models.py
@@ -1,17 +1,21 @@
 import numpy as np
 import pandas as pd
 import re
-import torch
-from transformers import AutoTokenizer
-from transformers import logging
 
-logging.set_verbosity_error()  # ignore transformer warning
+import torch
+import torch.nn.functional as F
+from transformers import AutoTokenizer
+from transformers import BertTokenizer
+from transformers import logging
+import multiprocessing
+
+logging.set_verbosity_error()  # ingnore transformer warning
 
 
 # kogpt model
 class Kogpt:
     # check model path
-    MODEL_PATH = "../ML_models/jm_model.pt"
+    MODEL_PATH = "../ML_models/jm_model.pt"  # manage.py 기준 상대 경로
     TOKENIZER_NAME = "skt/kogpt2-base-v2"
 
     def __init__(self):
@@ -49,9 +53,11 @@ class Kogpt:
 
         logits = outputs[0]
         logits = logits.detach().cpu()
+        logits = 0.9 * logits
         result = logits.argmax(-1)
+        print("kogpt", logits)
 
-        return np.array(result)[0]
+        return logits, np.array(result)[0]
 
 
 # koelectra model
@@ -91,11 +97,49 @@ class Koelectra:
         logits = outputs[0]
         logits = logits.detach().cpu()
         result = logits.argmax(-1)
+        print("koelectra", logits)
 
-        return np.array(result)[0]
+        return logits, np.array(result)[0]
+
+
+# kobert model
+class Kobert:
+    MODEL_PATH = "../ML_models/KoBERT_ver2.pt"
+
+    def __init__(self, device):
+        self.tokenizer = BertTokenizer.from_pretrained("monologg/kobert")
+        self.model = torch.load(self.MODEL_PATH, map_location=device)
+        self.model.eval()
+        self.device = device
+
+    def predict(self, sentence):
+        max_len = 64
+        self.model.eval()
+
+        encoding = self.tokenizer.encode_plus(
+            sentence,
+            add_special_tokens=True,
+            truncation=True,
+            max_length=max_len,
+            padding="max_length",
+            return_tensors="pt"
+        )
+
+        input_ids = encoding["input_ids"].squeeze().to(self.device)
+        attention_mask = encoding["attention_mask"].squeeze().to(self.device)
+
+        with torch.no_grad():
+            logits = self.model(input_ids.unsqueeze(0), attention_mask.unsqueeze(0))[0]
+            probabilities = F.softmax(logits, dim=-1).squeeze().cpu().numpy()
+            predicted_label = np.argmax(probabilities)
+
+        logits = logits.detach().cpu()
+        print("kobert", logits)
+        return logits, predicted_label
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+multiprocessing.set_start_method("spawn", True)
 
 # Load KOGPT
 kogpt = Kogpt()
@@ -103,12 +147,13 @@ kogpt = Kogpt()
 # Load KOELECTRA
 koelectra = Koelectra(device)
 
-
 # Load KOBERT
+kobert = Kobert(device)
 
 
 def get_sentence(json_content):
     target = json_content["images"][0][0]['fields'][0]['inferText']
+    print(target)
     start = False
     sentences = []
 
@@ -117,7 +162,7 @@ def get_sentence(json_content):
         content = d['inferText']
 
         if start == True:
-            if x < 900:
+            if x < 1440:
                 temp += content + " "
 
             else:
@@ -125,13 +170,14 @@ def get_sentence(json_content):
                 start = False
 
         else:
-            if x < 900 and content == target:
+            if x < 1440 and content == target:
                 start = True
                 temp = ""
 
     print(sentences)
     func = lambda s: s[:list(re.finditer(f"오전|오후", s))[-1].span()[0] - 1]
     sentences = [func(i) for i in sentences]
+    print(sentences)
     return sentences
 
 
@@ -142,7 +188,6 @@ def handle_received_json(json_content):
 
     j = pd.read_json(json_content, lines=True)
     sentences = get_sentence(j)
-    print(sentences)
 
     # Make Prediction
     label_decoder = {0: "sadness",
@@ -158,22 +203,24 @@ def handle_received_json(json_content):
     print(sentence)
 
     # KOGPT Prediction
-    kogpt_prediction = kogpt.predict(sentence)
+    kogpt_score, kogpt_prediction = kogpt.predict(sentence)
 
     # KOELECTRA Prediction
-    koelectra_prediction = koelectra.predict(sentence)
+    koelectra_score, koelectra_prediction = koelectra.predict(sentence)
 
     # KOBERT Prediction
-    kobert_prediction = 0
+    kobert_score, kobert_prediction = kobert.predict(sentence)
+
+    ensemble = kogpt_score + koelectra_score + kobert_score
+
+    print("ensemble", ensemble)
 
     print(f"kogpt_prediction : {kogpt_prediction} : {label_decoder[kogpt_prediction]}")
     print(f"koelectra_prediction : {koelectra_prediction} : {label_decoder[koelectra_prediction]}")
-    # print(f"kobert_prediction : {kobert_prediction} : {label_decoder[kobert_prediction]}")
+    print(f"kobert_prediction : {kobert_prediction} : {label_decoder[kobert_prediction]}")
 
     # ensemble
-    # prediction_list = [kogpt_prediction, koelectra_prediction, kobert_prediction]
-    prediction_list = [kogpt_prediction, koelectra_prediction]
-    ensemble_result = max(prediction_list, key=prediction_list.count)
+    ensemble_result = np.argmax(ensemble.numpy())
     print(f"ensemble_result : {ensemble_result} : {label_decoder[ensemble_result]}")
     print()
 
--- manage.py
+++ manage.py
@@ -2,6 +2,58 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+class BERTClassifier(nn.Module):
+    def __init__(self, bert, num_classes):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.num_classes = num_classes
+        self.classifier = nn.Linear(768, self.num_classes)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs.pooler_output
+        logits = self.classifier(pooled_output)
+        return logits
+
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.sentences = [data[0] for data in dataset]
+        self.labels = [int(data[1]) for data in dataset]  # Convert label to int
+        self.max_len = max_len
+
+    def __getitem__(self, idx):
+        sentence = self.sentences[idx]
+        label = self.labels[idx]
+
+        encoding = self.tokenizer.encode_plus(
+            sentence,
+            add_special_tokens=True,
+            truncation=True,
+            max_length=self.max_len,
+            padding="max_length",
+            return_tensors="pt"
+        )
+
+        input_ids = encoding["input_ids"].squeeze()
+        attention_mask = encoding["attention_mask"].squeeze()
+
+        return {
+            "input_ids": input_ids.to(device),
+            "attention_mask": attention_mask.to(device),
+            "label": torch.tensor(label, dtype=torch.long).to(device)
+        }
+
+    def __len__(self):
+        return len(self.labels)
 
 
 def main():
